9805064aa16d0c0226166d30dd18442cb640ac5a
bilalalpay <b.alpay7806@gmail.com>
2025-08-25 23:44:46 +0300

    31. Problem

--- try_field.py
+++ try_field.py
@@ -1,2 +1,12 @@
-for i in range(1,20,2):
-    print(i)
+
+target = int(input("Target: "))
+
+coins = [200, 100, 50, 20, 10, 5, 2, 1]  # 1 TL de eklendi
+ways = [0] * (target + 1)
+ways[0] = 1  # 0’a ulaşmanın 1 yolu vardır
+
+for coin in coins:
+    for value in range(coin, target + 1):
+        ways[value] += ways[value - coin]
+
+print(ways[target])

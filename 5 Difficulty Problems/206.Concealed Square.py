71563356bad29db0062b81bb2cf0f0e2f63ab986
bilalalpay <b.alpay7806@gmail.com>
2025-09-18 22:21:52 +0300

    206. problem çözümü

---
+++ 5 Difficulty Problems/206.Concealed Square.py
@@ -0,0 +1,31 @@
+
+def is_target(x):
+    x **= 2
+    if x % (10**5) < 80900 or x % (10**5) >= 90000:
+        return False
+    if x % (10**7) < 7080900 or x % (10**7) >= 8000000:
+        return False
+    if x % (10**9) < 607080900 or x % (10**9) >= 700000000:
+        return False
+    if x % (10**11) < 50607080900 or x % (10**11) >= 60000000000:
+        return False
+    if x % (10**13) < 4050607080900 or x % (10**13) >= 5000000000000:
+        return False
+    if x % (10**15) < 304050607080900 or x % (10**15) >= 400000000000000:
+        return False
+    if x % (10**17) < 20304050607080900 or x % (10**17) >= 30000000000000000:
+        return False
+    return True
+
+number = 10**9 + 30 
+while True:
+    if number % 100 == 30 or number % 100 == 70:
+        print(number)
+        if is_target(number):
+            print("FOUND: ",number)
+            break
+        number += 40
+        continue
+    number += 20
+
+#Very Slow
